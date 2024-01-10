655afe568095cd2ee32695612da36cf6a6ede335
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2024-01-10 06:54:25 +0100

    git subrepo clone --branch=master --force https://Cerenia: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@github.com/Cerenia/Simpill-subrepo.git app/src/main/java/com/example/simpill/ext
    subrepo:
      subdir:   "app/src/main/java/com/example/simpill/ext"
      merged:   "d146cdc"
    upstream:
      origin:   "https://Cerenia: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@github.com/Cerenia/Simpill-subrepo.git"
      branch:   "master"
      commit:   "d146cdc"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

---
+++ app/src/main/java/com/example/simpill/ext/AddOn.kt
@@ -0,0 +1,39 @@
+package com.example.simpill.ext
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
+import com.example.simpill.DatabaseHelper
+import com.example.simpill.ext.treatmentLog.TreatmentLogActivity
+import com.example.simpill.ext.treatmentLog.TreatmentLogDatabaseHelper
+
+/**
+ * To feed into python script, add around every function that is called from external repository.
+ */
+
+val MARKER_START = "IXwDmcyAEZEUvkES0IXy144JB SimPillAddOn start"
+val MARKER_END = "IXwDmcyAEZEUvkES0IXy144JB SimPillAddOn end"
+
+
+open class AddOn {
+    companion object {
+        @JvmStatic fun initializeGoToLogButton(button: Button, activity: Activity) : Unit {
+            button.setOnClickListener { openTreatmentLogActivity(activity) };
+        }
+
+        @JvmStatic fun openTreatmentLogActivity(context: Context) : Unit {
+            startActivity(context, Intent(context, TreatmentLogActivity::class.java), null)
+        }
+
+
+        @JvmStatic fun populateDatabases(tdb: TreatmentLogDatabaseHelper, pillDb: DatabaseHelper): Unit {
+            pillDb.populateWithDummies()
+            tdb.populateWithDummies(pillDb.allPills)
+        }
+
+        // Random comment for testing purposes
+    }
+}
+
